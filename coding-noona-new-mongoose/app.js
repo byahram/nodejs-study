cbd4ad246630c3343eebafbd2d474e109465bf2d
Ahram Kim <45866137+byahram@users.noreply.github.com>
2024-10-14 17:50:41 +0900

    코딩누나 NodeJS #4. 몽구스 테스트

---
+++ coding-noona-new-mongoose/app.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/mongoose-test");
+const validator = require("validator");
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: { type: String },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (value) {
+        if (!validator.isEmail(value)) 
+            throw new Error("This is not an Email");
+      },
+    },
+  },
+  password: { type: String, required: true, trim: true },
+  age: { type: Number, default: 0 },
+});
+
+const User = mongoose.model("User", userSchema);
+
+const newUser = new User({
+  name: "김철수",
+  email: "김철수@gmail.com",
+  password: "    dfg    12345",
+});
+
+// newUser.save().then((value) => console.log("value is", value));
+
+User.find({name: "김철수 -_id"}).select("name").then((value) => console.log("all data", value))
