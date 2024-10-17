37a8f6b18e63b9c32ff1d76f67eefbf639b92ad2
Ahram Kim <45866137+byahram@users.noreply.github.com>
2024-10-17 21:01:52 +0900

    코딩누나 NodeJS #6. 할일앱 만들기 5~8

--- coding-noona-todolist/backend/app.js
+++ coding-noona-todolist/backend/app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
 
+require("dotenv").config();
+
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 app.use("/api", indexRouter);
 
-const mongoURI = `mongodb://localhost:27017/todolist`;
+const mongoURI = process.env.MONGODB_URI_PROD;
 
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
